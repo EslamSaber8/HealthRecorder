bf1acca010232d83fd0ef7134e9149363e1c6e5f
EslamSaber8 <islamsaper5@gmail.com>
2023-05-18 20:38:01 +0300

    abdalslam

--- routes/doctorRoutes.js
+++ routes/doctorRoutes.js
@@ -1,4 +1,17 @@
 const express = require('express');
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE, OPTIONS"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
 const doctorController = require('../controllers/doctorController');
 const doctorAuthController=require("../controllers/doctorAuthController");
 const router = express.Router();
